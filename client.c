e2b47571584efa1b58bc5bd93c0921a77561d7d4
npapoutsakis <nikospapoutsak@gmail.com>
2025-04-30 17:16:02 +0300

    made the server-client connection

--- client.c
+++ client.c
@@ -4,14 +4,65 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
-
+#define DEFAULT_IP "147.27.4.22"
+#define PORT 5000
+#define BUFFER_SIZE 1024
 
 int main(int argc, char *argv[]) {
 
+    int client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (client_socket == -1) {
+        printf("Socket creation failed...\n");
+        exit(EXIT_FAILURE);
+    }
 
+    // set up the client address structure
+    struct sockaddr_in client_addr;
+    
+    memset(&client_addr, 0, sizeof(client_addr));
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_addr.s_addr = inet_addr(DEFAULT_IP);
+    // client_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    client_addr.sin_port = htons(PORT);
+    
+    // attempt to connect to the server    
+    if (connect(client_socket, (struct sockaddr *)&client_addr, sizeof(client_addr)) == -1) {
+        printf("Connection to server failed...\n");
+        exit(EXIT_FAILURE);
+    }
+    
 
+    char buffer[BUFFER_SIZE];
+    int n;
+    
+    // main client loop
+    printf("Connected to server. Type 'exit' to quit.\n");
+    while (1) {
+        bzero(buffer, BUFFER_SIZE);
+        printf("Type a message: ");
+        n = 0;
+        while ((buffer[n++] = getchar()) != '\n');
+        
+        send(client_socket, buffer, sizeof(buffer), 0);
+        bzero(buffer, BUFFER_SIZE);
+        
+        recv(client_socket, buffer, sizeof(buffer), 0);
+        printf("From server: %s", buffer);
 
+        if (strncmp(buffer, "exit", 4) == 0) {
+            printf("Exiting...\n");
+            break;
+        }
+    }
+
+    close(client_socket);
+    printf("Connection closed.\n");
     return 0;
 }
--- server.c
+++ server.c
@@ -4,12 +4,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 // see available ports: cat /etc/services
 #define PORT 5000
+#define BUFFER_SIZE 1024
+
 
 int main(int argc, char *argv[]) {
     
@@ -40,16 +45,63 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    // listen for incoming connections
-    if (listen(server_socket, 5) == -1) {
+    // listen for incoming connections, 1 is the maximum number of queued connections
+    if (listen(server_socket, 1) == -1) {
         printf("Socket listening failed...\n");
         exit(EXIT_FAILURE);
     }
 
+    printf("Server listening on port %d\n", PORT);
 
+    
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    
+    // accept a connection from a client
+    int client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_len);
+    if (client_socket == -1) {
+        printf("Socket accepting failed...\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    // print the client's ip address and port
+    printf("Client connected: %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+    
+    char buffer[BUFFER_SIZE];
+    int n;
 
+    // main server loop
+    while (1) {
+        
+        // clear buffer
+        bzero(buffer, BUFFER_SIZE);
 
+        // receive data from the client, returns the number of bytes received
+        ssize_t bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+        if (bytes_received <= 0) {
+            printf("Client disconnected...\n");
+            break;
+        }
 
+        // print the received data
+        printf("From client: %s\t To client : ", buffer);
+        bzero(buffer, BUFFER_SIZE);
+        
+        n = 0; 
+        while ((buffer[n++] = getchar()) != '\n'); 
+    
+        // send data to the client
+        send(client_socket, buffer, strlen(buffer), 0);
 
+        // close the client socket using `exit`
+        if (strncmp("exit", buffer, 4) == 0) { 
+            printf("Server Exit...\n"); 
+            break; 
+        } 
+    }
+
+    // close the sockets
+    close(server_socket);
+    printf("Server closed.\n");
     return 0;
 }
