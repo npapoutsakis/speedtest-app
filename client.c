dde8935b9bbd352733f67d8cfc72f6986d9835e6
npapoutsakis <nikospapoutsak@gmail.com>
2025-05-01 22:35:49 +0300

    testing ./client <server_ip>

--- client.c
+++ client.c
@@ -16,6 +16,18 @@
 
 int main(int argc, char *argv[]) {
 
+    if (argc != 2) {
+        printf("Usage: %s <server_ip>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // parse the server IP address
+    char *server_ip = argv[1];
+    if (server_ip == NULL || strlen(server_ip) == 0) {
+        printf("Invalid server IP address. Using default: %s\n", DEFAULT_IP);
+        server_ip = DEFAULT_IP;
+    }    
+
     int client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (client_socket == -1) {
         printf("Socket creation failed...\n");
@@ -27,8 +39,7 @@ int main(int argc, char *argv[]) {
     
     memset(&client_addr, 0, sizeof(client_addr));
     client_addr.sin_family = AF_INET;
-    client_addr.sin_addr.s_addr = inet_addr(DEFAULT_IP);
-    // client_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    client_addr.sin_addr.s_addr = inet_addr(server_ip);
     client_addr.sin_port = htons(PORT);
     
     // attempt to connect to the server    
@@ -40,9 +51,26 @@ int main(int argc, char *argv[]) {
     
     // main client loop
     while (1) {
+    
+        char buffer[1024];
+        printf("[CLIENT] Enter message to send to server (or 'exit' to quit): ");
+        memset(buffer, 0, sizeof(buffer));
         
-        
+        int n = 0;
+        while ((buffer[n++] = getchar()) != '\n');
 
+        // check for exit command
+        if (strcmp(buffer, "exit") == 0) {
+            break;
+        }
+
+        // send message to server
+        send(client_socket, buffer, strlen(buffer), 0);
+        memset(buffer, 0, sizeof(buffer));
+
+        // receive response from server
+        recv(client_socket, buffer, sizeof(buffer), 0);
+        printf("Received from server: %s\n", buffer);
 
     }
 
